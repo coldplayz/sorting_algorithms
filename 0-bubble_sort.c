fe3bd912c57a33f7893f263eee2ff0caac350c0d
Greenbel <greenbel.chibuike@gmail.com>
2022-11-04 20:41:28 +0100

    Update.

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,39 +1,40 @@
 #include "sort.h"
 
 /**
- * swap - swaps to integers
- * @a: First integer
- * @b: Second integer
- * Return: Nothing
- */
-void swap(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-/**
- * bubble_sort - Sorts an array in ascending order
- * @array: Array to be sorted
- * @size: size of the array
- * Return: Nothing
+ * bubble_sort - sorts an array of integers in ascending order.
+ * @array: the int array to sort.
+ * @size: the number of integers in @array.
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	int j, tmp, size_cpy, swap = 0;
+	size_t i;
 
-	for (i = 0; i < size - 1; i++)
+	if (size >= 2)
 	{
-		for (j = 0; j < size; j++)
+		size_cpy = size;
+		for (i = 0; i < size; i = 0)
 		{
-			if (array[j] > array[j + 1])
+			for (j = 1; j < size_cpy; j++)
 			{
-				swap(&array[j], &array[j + 1]);
-				print_array(array, size);
+				if (array[i] > array[j])
+				{
+					/* Swap values */
+					tmp = array[i];
+					array[i] = array[j];
+					array[j] = tmp;
+					swap = 1;
+					print_array(array, size);
+				}
+				i++;
 			}
+			if (!swap || size_cpy == 2)
+			{
+				/* No sort this loop, or final sort just occured */
+				return;
+			}
+			swap = 0;
+			size_cpy--;
 		}
 	}
 }
