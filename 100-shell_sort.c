e1b4d62ec1d7645b6c9af2c5030a07a3683cd0e0
= <=>
2022-11-06 17:05:01 +0000

    Added shell sort and quick sort to new feature

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,84 +1,52 @@
 #include "sort.h"
 
-void sort_stream(
-		int *start_a, int *curr_stream, int *array, size_t size, int gap);
+/**
+ * swap - Swaps two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Nothing
+ */
+void swap(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 
 /**
- * shell_sort - implements Shell Sort algorithm with the Knuth sequence of gaps
- * @array: the array of integers to sort.
- * @size: number of items in @array.
+ * shell_sort - Sorts an array using the shell sort
+ * algorithm and knuth sequence
+ *
+ * @array: Array to be sorted
+ * @size: Size of array
+ * Return: Nothing
  */
 void shell_sort(int *array, size_t size)
 {
-	int i, k, *start_a, gaps[] = {4, 1, 0};
-	size_t j;
+	size_t gap = 1, i, j;
 
-	if (size < 2 || !array)
-	{
+	if (size < 2)
 		return;
-	}
 
-	/* Iterate through array of gaps */
-	for (i = 0; gaps[i]; i++)
+	while (gap < size / 3)
 	{
-		/* Iterate for as many sub-arrays as there would be */
-		for (k = 0; k < gaps[i]; k++)
+		gap = 3 * gap + 1;
+	}
+	while (gap >= 1)
+	{
+		for (i = gap; i < size; i++)
 		{
-			/* Produce and sort the stream */
-			for (j = 0; j < size; j = j + gaps[i])
+			for (j = i; j >= gap; j -= gap)
 			{
-				if (j == 0)
+				if (array[j] < array[j - gap])
 				{
-					start_a = array + k;
-					j = k; /* reset to the index of the first element of the current array */
-					continue; /* stream-sorting to start from next stream index */
+					swap(&array[j], &array[j - gap]);
 				}
-				sort_stream(start_a, array + j, array, size, gaps[i]);
 			}
 		}
+		gap /= 3;
 		print_array(array, size);
-	}
-}
-
-
-
-
-
-
-/**
- * sort_stream - implements insertion sort for a stream of inputs.
- * @start_a: address of first element of stream's array.
- * @curr_stream: address of current array item being streamed in.
- * @array: array to sort.
- * @size: size of @array.
- * @gap: integer representing the
- * current gap between elements in a streaming array.
- */
-void sort_stream(
-		int *start_a, int *curr_stream, int *array, size_t size, int gap)
-{
-	int tmp_v, *prev_stream;
-
-	(void)size;
-	(void)array;
-	prev_stream = curr_stream - gap; /* initialize prev_stream */
-	for (; curr_stream != start_a;)
-	{
-		if (*curr_stream > *prev_stream)
-		{
-			return;
-		}
-		/* Swap, pushing values forward */
-		tmp_v = *curr_stream;
-		*curr_stream = *prev_stream;
-		*prev_stream = tmp_v;
-		/* Update addresses */
-		curr_stream = curr_stream - gap;
-		if (curr_stream == start_a)
-		{
-			/* Start of array; return to avoid assigning illegal address to prev_str */
-			return;
-		}
-		prev_stream = prev_stream - gap;
 	}
 }
