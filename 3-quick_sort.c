dcb13c1e135f0a3968bea921d0be04e2b801be51
Greenbel <greenbel.chibuike@gmail.com>
2022-11-07 22:25:13 +0100

    Re-implemented Quick Sort.

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,86 +1,114 @@
 #include "sort.h"
 
-/**
- * swap - Swaps two integers
- * @a: First integer
- * @b: Second integer
- * Return: Nothing
- */
-void swap(int *a, int *b)
-{
-	int tmp;
 
-	tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
+void lomuto_sort(int *array, size_t size, int *low_a, int *high_a);
+
 
 /**
- * partition - Divides an array into two sub arrays at
- * pivot point such numbers to the left of pivot are
- * <= pivot while those to the right are > pivot
- *
- * @array: Array to be partitioned
- * @lo: Lower boundary of array
- * @hi: Upper boundary of array
- * Return: Index to the pivot value
- */
-int partition(int *array, int lo, int hi)
-{
-	int i, j, pivot, size;
-
-	size = hi + 1;
-
-	/*Applying the lamuto technique of setting*/
-	/*the upper bound value as pivot*/
-	pivot = array[hi];
-
-	/*Keeps boundary of last index <= pivot*/
-	i = lo;
-
-	for (j = lo; j < hi; j++)
-	{
-		if (array[j] <= pivot)
-		{
-			swap(&array[i], &array[j]);
-			print_array(array, size);
-			i++;
-		}
-	}
-	swap(&array[i], &array[hi]);
-	print_array(array, size);
-	return (i);
-}
-
-/**
- * quick_sort_ - Sorts an array using quick sort algorithm
- * this version of quick sort takes 3 arguments
- *
- * @array: Array to be sorted
- * @lo: Lower boundary of array
- * @hi: Upper boundary of array
- * Return: Nothing
- */
-void quick_sort_(int *array, int lo, int hi)
-{
-	int pivot;
-
-	if (lo < hi)
-	{
-		pivot = partition(array, lo, hi);
-		quick_sort_(array, lo, pivot - 1);
-		quick_sort_(array, pivot + 1, hi);
-	}
-}
-/**
- * quick_sort - Sorts an array using quick sort algorithm
- * this version of quick sorted uses 2 arguments
- *
- * @array: Array to be sorted
- * @size: Size of the array
- * Return: Nothing
+ * quick_sort - implements Quick Sort using the Lamuto partitioning scheme.
+ * @array: the array to sort.
+ * @size: the number of integers in @array.
  */
 void quick_sort(int *array, size_t size)
 {
-	quick_sort_(array, 0, size - 1);
+	int *low_a, *high_a;
+
+	if (size < 2 || !array)
+	{
+		return;
+	}
+
+	low_a = array + 0; /* start of array */
+	high_a = array + (size - 1); /* end of array */
+
+	lomuto_sort(array, size, low_a, high_a);
+}
+
+
+
+
+/**
+ * lomuto_sort - implements Lomuto partitioning and sort.
+ * @array: array of integers being sorted.
+ * @size: size of @array.
+ * @low_a: start address of the (sub-)array to partition and sort.
+ * @high_a: last address of the (sub-)array to partition and sort.
+ */
+void lomuto_sort(int *array, size_t size, int *low_a, int *high_a)
+{
+	int *partition_a, *pivot_a, *cmp_a, at_pivot = 0;
+
+	if (low_a == high_a)
+		return; /* only one item in subarray */
+
+	pivot_a = high_a;
+	partition_a = low_a;
+	cmp_a = low_a;
+
+	while (cmp_a != pivot_a)
+	{
+		if (*pivot_a < *cmp_a)
+		{
+			/* Probe for value less than pivot's to swap with partition's */
+			while (1)
+			{
+				/* Loop will run till cmp_a == pivot_a */
+				cmp_a++; /* move to next comparison address */
+				if (cmp_a == pivot_a)
+				{
+					at_pivot = 1;
+					break;
+				}
+				if (*cmp_a < *pivot_a)
+				{
+					/* Swap with value at partition_a */
+					swap(cmp_a, partition_a);
+					print_array(array, size);
+					partition_a++;
+				}
+			}
+		}
+		if (at_pivot)
+			break; /* swapping between pivot and partition to happen outside */
+		cmp_a++, partition_a++;
+	}
+	if (pivot_a == partition_a)
+	{
+		/* No swapping due, and no subarray on right, only left */
+		lomuto_sort(array, size, low_a, partition_a - 1);
+	}
+	if (at_pivot)
+	{
+		/* Swap pivot and partition values */
+		swap(pivot_a, partition_a);
+		print_array(array, size);
+	}
+	/* Partition at partition_a */
+	if (low_a != partition_a)
+	{
+		/* Valid items on the left of partition point */
+		lomuto_sort(array, size, low_a, partition_a - 1);
+	}
+	if (high_a != partition_a)
+	{
+		/* Valid items on the left */
+		lomuto_sort(array, size, partition_a + 1, high_a);
+	}
+}
+
+
+
+
+/**
+ * swap - exchanges values between two addresses.
+ * @add1: a pointer to int.
+ * @add2: a pointer to int.
+ */
+void swap(int *add1, int *add2)
+{
+	int tmp_v;
+
+	tmp_v = *add1;
+	*add1 = *add2;
+	*add2 = tmp_v;
 }
